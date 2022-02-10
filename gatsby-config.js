a3bf8c8f0d877287da9d71e16a551cb76ae9068a
Charles Cole <smashdaisaku@gmail.com>
2022-02-10 22:33:33 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+        url: `http://workapital.local/graphql`,
       },
     },
 
